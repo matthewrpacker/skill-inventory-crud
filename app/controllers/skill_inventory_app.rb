6eeaad240ec585403d03019535ca6646ffb17e10
matthewrpacker <matthewrpacker@gmail.com>
2016-06-29 09:32:47 -0600

    initial commit

--- app/controllers/skill_inventory_app.rb
+++ app/controllers/skill_inventory_app.rb
@@ -1,8 +1,27 @@
 class SkillInventoryApp < Sinatra::Base
   set :root, File.join(File.dirname(__FILE__), '..')
 
+  get '/' do
+    erb :dashboard
+  end
+
   get '/skills' do
+    @skills = SkillInventory.all
     erb :index
+  end
+
+  get '/skills/new' do
+    erb :new
+  end
+
+  post '/skills' do
+    SkillInventory.create(params[:skill])
+    redirect '/skills'
+  end
+
+  get '/skills/:id' do |id|
+    @skill = SkillInventory.find(id.to_i)
+    erb :show
   end
 
 end
